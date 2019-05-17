3a9f8b74f16f2bff99c4c636076561980f312463
Разгуляева Анастасия Дмитриевна OUT <Razgulyaeva.An.Dm@sberbank.ru>
2019-05-17 11:21:02 +0300

    Maven + чат

---
+++ src/main/java/com/project/chat/network/TCPConnection.java
@@ -0,0 +1,69 @@
+package com.project.chat.network;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class TCPConnection {
+
+    private final Socket socket;
+    private final Thread rxThread;
+    private final BufferedReader in;
+    private final BufferedWriter out;
+    private final TCPConnectionListener eventListener;
+
+    public TCPConnection(TCPConnectionListener eventListener, String ipAddr, int port) throws IOException {
+        this(eventListener, new Socket(ipAddr, port));
+    }
+
+    public TCPConnection(TCPConnectionListener tcpConnectionListener, Socket socket) throws IOException {
+        this.eventListener = tcpConnectionListener;
+        this.socket = socket;
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
+        this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    eventListener.onConnectionReady(TCPConnection.this);
+                    while(!rxThread.isInterrupted()) {
+                        eventListener.onReceiveString(TCPConnection.this, in.readLine());
+                    }
+                } catch (IOException e) {
+                    eventListener.onException(TCPConnection.this, e);
+                } finally {
+                    eventListener.onDisconnect(TCPConnection.this);
+                }
+            }
+        });
+        rxThread.start();
+    }
+
+    public synchronized void sendString(String value) {
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+            disconnect();
+        }
+    }
+
+    public synchronized void disconnect() {
+        rxThread.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
+    }
+}
---
+++ src/main/java/com/project/chat/server/ChatServer.java
@@ -0,0 +1,63 @@
+package com.project.chat.server;
+
+import com.project.chat.network.TCPConnection;
+import com.project.chat.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer implements TCPConnectionListener {
+
+    private static final int SERVER_PORT = 8189;
+    private final List<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private void sendToAllConnections(String value) {
+        System.out.println(value);
+        for (TCPConnection connection : connections) {
+            connection.sendString(value);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+}
